88be6981e1868814dae1435cd855428b6cbb967b
duongbinh2001 <66376290+duongbinh2001@users.noreply.github.com>
2020-09-09 20:44:36 +0700

    bt

---
+++ App2/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            string email = Email.Text;
+            TxtBlock.Text += email + "\n";
+            Email.Text = "";
+
+            string content = Content.Text;
+            TxtBlock.Text += content + "\n";
+            Email.Text = "";
+
+            string title = Title.Text;
+            TxtBlock.Text += title + "\n";
+            Title.Text = "";
+
+        }
+
+        private void Email_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ App2/Models/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2.Models
+{
+    public class Class1
+    {
+        private string email;
+        private string title;
+        private string content;
+
+        public Class1(string email, string title, string content)
+        {
+            Email = email;
+            Title = title;
+            Content = content;
+        }
+        public string Email
+        {
+            get => email;
+            set => email = value;
+        }
+        public string Title
+        {
+            get => title;
+            set => title = value;
+        }
+        public string Content
+        {
+            get => content;
+            set => content = value;
+        }
+        public string ToString()
+        {
+            return Email + "--" + Title + "--" + Content.ToString() + "\n";
+        }
+    }
+}
